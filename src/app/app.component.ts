7e64bd14ee7e7c0bdc98754c8010bce5b5284928
abdulqayoom <hqayoom@gmail.com>
2023-08-21 16:04:13 +0530

    git changes

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { dummy } from './dummy';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular';
+ public user:dummy
+
+
+constructor(){
+
+  this.user={
+    id:2,
+    name:"hafil",
+    age:22
+  
+  }
+  console.log(this.user)
+  console.log(this.user)
+}
+
+
+}
---
+++ src/app/dummy.ts
@@ -0,0 +1,6 @@
+export  class dummy{
+    id!:number
+    name!:string
+    age!:number
+
+}
